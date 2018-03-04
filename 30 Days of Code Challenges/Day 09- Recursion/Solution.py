add1af0f757bc378aa8cc7cb5dbbd3fbaf8774bb
Hamzah Khan <khamzah22@gmail.com>
2018-03-04 13:20:50 +0530

    Create Solution.py

---
+++ 30 Days of Code Challenges/Day 09- Recursion/Solution.py
@@ -0,0 +1,14 @@
+import sys
+
+def factorial(n):
+    # Complete this function
+    if n < 1:
+        return 1
+    else:
+        inter = n * factorial(n-1)
+        return inter
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    result = factorial(n)
+    print(result)
